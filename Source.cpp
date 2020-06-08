5f8efef9901328cb64f8b8e1afc9bd656c9207f2
Микола Матіос <mikolka09@gmail.com>
2020-06-09 00:24:28 +0300

    09.06.2020

--- Source.cpp
+++ Source.cpp
@@ -9,17 +9,23 @@
 using namespace std;
 
 
+
 int main()
 {
 	setlocale(0, "");
 
-	//Time t1(20, 100, 200);
-	//Time t2(10, 200, 500);
-	Time t, t3;
-	t3 = t(10, 15, 20, 15, 13, 25);
+	
+	Time t1(20, 100, 200);
+	Time t2(10, 200, 500);
+	Time t;
+	//t3 = t(10, 15, 20, 15, 13, 25);
 	//cout << (t1 != t2) << endl;
-	t3.printTo12();
-	t3.printTo24();
+	///t3 = t1 * 3;
+	//t3.printTo12();
+	t(t1, 24);
+	t(t1, 12);
+	//t.printTo24();
+
 
 
 
--- Time.h
+++ Time.h
@@ -15,12 +15,12 @@ public:
 
 	Time operator+ (Time t2);
 	Time operator- (Time t2);
-	Time operator* (Time t2);
+	Time operator* (int k);
 	bool operator> (Time t2);
 	bool operator< (Time t2);
 	bool operator== (Time t2);
 	bool operator!= (Time t2);
-	Time operator() (int h1, int m1, int s1, int h2, int m2, int s2);
+	void operator() (Time t, int f);//перегрузка вывода времени с укзанием формата
 
 	void printTo12() const;
 	void printTo24() const;
@@ -68,12 +68,12 @@ inline Time Time::operator-(Time t2)
 	return t;
 }
 
-inline Time Time::operator*(Time t2)
+inline Time Time::operator*(int k)
 {
 	Time t;
-	t.h = this->h * t2.h;
-	t.m = this->m * t2.m;
-	t.s = this->s * t2.s;
+	t.h = this->h * k;
+	t.m = this->m * k;
+	t.s = this->s * k;
 	return t;
 }
 
@@ -101,17 +101,67 @@ inline bool Time::operator!=(Time t2)
 	return (this->h * 3600 + this->m * 60 + this->s) != (t2.h * 3600 + t2.m * 60 + t2.s);
 }
 
-inline Time Time::operator()(int h1, int m1, int s1, int h2, int m2, int s2)
+inline void Time::operator()(Time t, int f)
 {
-	Time t1, t2, t;
-	t1.h = h1;
-	t1.m = m1;
-	t1.s = s1;
-	t2.h = h2;
-	t2.m = m2;
-	t2.s = s2;
-	t = t1 - t2;
-	return t;
+	int s, sek, min, hou;
+	s = t.s + t.m * 60 + t.h * 3600;
+	hou = s / 3600;
+	min = (s - hou * 3600) / 60;
+	sek = s - hou * 3600 - min * 60;
+	if (f == 12)
+	{
+		cout << "\n\n";
+		cout << " Время в 12 часовом формате: ";
+		if (hou > 24)
+		{
+			int d;
+			d = hou / 24;
+			hou = hou - d * 24;
+		}
+		if (hou >= 0 && hou < 12)
+		{
+			hou < 10 ? cout << "0" << hou : cout << hou;
+			cout << ":";
+			min < 10 ? cout << "0" << min : cout << min;
+			cout << ":";
+			sek < 10 ? cout << "0" << sek : cout << sek;
+			cout << " AM" << endl;
+			cout << endl;
+		}
+		else
+		{
+			if (hou >= 12 && hou < 24)
+			{
+				if (hou > 12)
+					(hou - 12) < 10 ? cout << "0" << hou - 12 : cout << hou - 12;
+				else
+					cout << hou;
+				cout << ":";
+				min < 10 ? cout << "0" << min : cout << min;
+				cout << ":";
+				sek < 10 ? cout << "0" << sek : cout << sek;
+				cout << " PM" << endl;
+				cout << endl;
+			}
+		}
+	}
+	else if (f == 24)
+	{
+		cout << "\n\n";
+		cout << " Время в 24 часовом формате: ";
+		if (hou > 24)
+		{
+			int d;
+			d = hou / 24;
+			hou = hou - d * 24;
+		}
+		hou < 10 ? cout << "0" << hou : cout << hou;
+		cout << ":";
+		min < 10 ? cout << "0" << min : cout << min;
+		cout << ":";
+		sek < 10 ? cout << "0" << sek : cout << sek << endl;
+		cout << endl;
+	}
 }
 
 inline void Time::printTo12() const
@@ -122,7 +172,7 @@ inline void Time::printTo12() const
 	min = (t - hou * 3600) / 60;
 	sek = t - hou * 3600 - min * 60;
 	cout << "\n\n";
-	cout << " Введенное время в 12 часовом формате: ";
+	cout << " Время в 12 часовом формате: ";
 	if (hou > 24)
 	{
 		int d;
@@ -166,7 +216,7 @@ inline void Time::printTo24() const
 	min = (t - hou * 3600) / 60;
 	sek = t - hou * 3600 - min * 60;
 	cout << "\n\n";
-	cout << " Введенное время в 24 часовом формате: ";
+	cout << " Время в 24 часовом формате: ";
 	if (hou > 24)
 	{
 		int d;
